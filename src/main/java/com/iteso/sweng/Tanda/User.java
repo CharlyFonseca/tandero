22f0252b1c8b02ce451339111bab3680c6f24e3b
CharlyFonseca <is688302@iteso.mx>
2014-10-15 19:49:54 -0500

    6 pts ftw

--- src/main/java/com/iteso/sweng/Tanda/User.java
+++ src/main/java/com/iteso/sweng/Tanda/User.java
@@ -4,4 +4,16 @@ package com.iteso.sweng.Tanda;
  * Created by Charly on 15/10/2014.
  */
 public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
 }
